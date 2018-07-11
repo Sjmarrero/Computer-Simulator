ebe4673cd697f9f804c9f1c5eff3a85320ef021f
Sjmarrero <33099245+Sjmarrero@users.noreply.github.com>
2018-07-11 11:07:38 -0700

    Update Computer.java

--- Computer.java
+++ Computer.java
@@ -19,19 +19,54 @@ public class Computer implements ComputerInterface {
 	
 	public void build() {
 		
-		Register ra;
+		Register r0;
 		
-		Register rb;
+		Register r1;
 		
-		Register rc;
+		Register r2;
 		
-		Register rd;
+		Register r3;
 		
 	}
 	
 	public void run() {
 		
+		//READ
+		readInsturction();
+		//MOVE R0, R1
 		
+		//READ
+		
+		//MOVE R0, R2
+		
+		//ADD R1, R2, R3
+		
+		//MOVE R3, R0
+		
+		//PRINT
+		
+		//READ
+		
+		//MOVE R0, R1
+		
+		//SUB R1, R3, R0
+		
+		//PRINT
+		
+		//READ
+		
+		//MOVE R0, R2
+		
+		//ADD R3, R2, R1
+		
+		//MOVE R1, R0
+		
+		//PRINT
+	}
+	
+	private void readInstruction(){
+		reader.storeInteger();
+		r0.setInteger(reader.getInteger());
 	}
 
 }
