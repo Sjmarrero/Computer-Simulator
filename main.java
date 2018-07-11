7af2b0647fe590b0cd9c2e6399a40a00df508a91
poudyalsaroz <33234259+poudyalsaroz@users.noreply.github.com>
2018-07-11 10:41:48 -0700

    Update main.java

--- main.java
+++ main.java
@@ -4,6 +4,13 @@ public class main {
 
 	public static void main(String[] args) {
 		
+		public static void main(String[] args) {
+		
+		Computer comp  = new Computer();
+		
+		comp.run();
+	}
+		
 	}
 
 }
